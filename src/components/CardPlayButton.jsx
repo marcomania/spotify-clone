dc2d3fb4c6575e5fa45a52e4b9632566cdf91a18
Marco Untiveros <marco.untiveros@unmsm.edu.pe>
2023-12-04 20:23:14 +0000

    feat: :lipstick: Add play/pause button  in cards

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,10 @@
+import { Play } from "@/components/Player";
+
+
+export function CardPlayButton ({ id }) {
+    return (
+        <div className="card-play-button rounded-full bg-green-500 p-4">
+            <Play/>
+        </div>
+    )
+}
