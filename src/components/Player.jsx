31ec12237229f1c4bb4f1252849a44f1d25ccd0a
Marco Untiveros <marco.untiveros@unmsm.edu.pe>
2023-12-04 05:08:32 +0000

    feat: :sparkles: Audio Persistence

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const Pause = ({ className }) => (
     <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -10,6 +10,24 @@ export const Pause = ({ className }) => (
 
   export function Player() {
     const [isPlaying, setIsPlaying] = useState(false)
+    const [CurrentSong, setCurrentSong] = useState(null)
+    const audioRef = useRef()
+
+    useEffect(() => {
+        audioRef.current.src = `/music/2/02.mp3`
+    }, [])
+
+    const handleClick = () => {
+        if(isPlaying){
+            audioRef.current.pause()
+        } else {
+            audioRef.current.play() 
+            audioRef.current.volume = 0.1
+        }
+        
+        setIsPlaying(!isPlaying)
+    }
+
     return (
         <div className="flex flex-row justify-between w-full px-4 z-50">
             <div>
@@ -17,7 +35,7 @@ export const Pause = ({ className }) => (
             </div>
             <div className="grid place-content-center gap-4 flex-1">
                 <div className="flex justify-center">
-                    <button className="bg-white rounded-full p-2" onClick={() => setIsPlaying(!isPlaying)}>
+                    <button className="bg-white rounded-full p-2" onClick={handleClick}>
                         {isPlaying ? <Pause/> : <Play/> }
                     </button>
                 </div>
