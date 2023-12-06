8bb4a481a6b701c246b07fefe1f2a89de53e60cc
Marco Untiveros <marco.untiveros@unmsm.edu.pe>
2023-12-06 05:51:54 +0000

    feat: :sparkles: Showing current song .
    showing current song in player area.

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -3,13 +3,23 @@ import { usePlayerStore } from "@/store/playerStore";
 
 export function CardPlayButton ({ id }) {
     const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
-    const handleClick = () => {
-        setCurrentMusic({
-            playlist: { id }
-        });
-        setIsPlaying(!isPlaying);
-    }
+
     const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
+
+    const handleClick = () => {
+        if(isPlayingPlaylist) {
+            setIsPlaying(false)
+            return
+        }
+        fetch(`/api/get-info-playlist.json?id=${id}`)
+            .then(res => res.json())
+            .then(data => {
+                const {songs, playlist } = data
+
+                setIsPlaying(true)
+                setCurrentMusic({ songs, playlist, song: songs[0]})
+            }) 
+    }
 
     return (
         <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,31 +9,53 @@ export const Pause = ({ className }) => (
     <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
   )
 
-  export function Player() {
-    const { isPlaying , setIsPlaying } = usePlayerStore(state => state)
+const CurrentSong = ({ image, title , artists}) => {
+    return (
+        <div className="flex items-center gap-5 relative overflow-hidden">
+            <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden"> 
+                <img src={image} alt={title}/>
+            </picture>
+            <div className="flex flex-col">
+                <h3 className="font-semibold text-sm block">
+                    {title}
+                </h3>
+                <span className="text-xs opacity-80 ">
+                    {artists?.join(', ') }
+                </span>
+            </div>
 
-    const [CurrentSong, setCurrentSong] = useState(null)
+        </div>
+    )
+}
+
+export function Player() {
+    const { currentMusic, isPlaying , setIsPlaying } = usePlayerStore(state => state)
+
     const audioRef = useRef()
+    useEffect(() => {
+        isPlaying
+            ? audioRef.current.play()
+            : audioRef.current.pause()
+    }, [isPlaying])
 
     useEffect(() => {
-        audioRef.current.src = `/music/2/02.mp3`
-    }, [])
+        const {song, playlist, songs} = currentMusic
+
+        if(song){
+            const src = `/music/${playlist?.id}/0${song.id}.mp3`
+            audioRef.current.src = src
+            audioRef.current.play()
+        }
+    }, [currentMusic])
 
     const handleClick = () => {
-        if(isPlaying){
-            audioRef.current.pause()
-        } else {
-            audioRef.current.play() 
-            audioRef.current.volume = 0.1
-        }
-        
         setIsPlaying(!isPlaying)
     }
 
     return (
         <div className="flex flex-row justify-between w-full px-4 z-50">
             <div>
-                CurrentSong...
+                <CurrentSong {...currentMusic.song}/>
             </div>
             <div className="grid place-content-center gap-4 flex-1">
                 <div className="flex justify-center">
